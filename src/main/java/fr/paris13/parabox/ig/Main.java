be93e717f153b1a08aa539675fe26bcb08451ce9
Léa Luu <lea.luu@edu.univ-paris13.fr>
2026-04-06 18:10:21 +0200

    Ajout d'une classe Level pour afficher un niveau et d'un Main pour test

--- src/main/java/fr/paris13/parabox/ig/Main.java
+++ src/main/java/fr/paris13/parabox/ig/Main.java
@@ -1,12 +1,10 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
-import javafx.geometry.Pos;
-
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 
 public class Main extends Application {
 
@@ -19,19 +17,84 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Parabox Game");
+        stage.setTitle("HerTech Game");
+
+
+
+        // afficher page accueil
+        // boutons pr choisir mode if ite : menu avec liste niveau if rec : niveau initial
+
+
 
         // Test
-        VBox home = new VBox();
-        Label etiquette = new Label("BLABLA");
-        Button salut = new Button("Salut");
-        Button bye = new Button("Bye");
-        home.getChildren().addAll(etiquette, salut, bye);
-        home.setStyle("-fx-background-color: pink;");
-        home.setAlignment(Pos.CENTER);
+        Grille g = new Grille(10, 7, "Niveau 5");
+        g.setObjet(new Mur(0, 1, g), 0, 1);
+        g.setObjet(new Mur(3, 2, g), 3, 2);
+        g.setObjet(new Mur(6, 2, g), 6, 2);
+        g.setObjet(new Mur(6, 3, g), 6, 3);
+        g.setObjet(new Mur(6, 4, g), 6, 4);
+        g.setObjet(new Mur(3, 5, g), 3, 5);
+        g.setObjet(new Cible(8, 1, g), 8, 1);
+        g.setObjet(new Cible(8, 3, g), 8, 3);
+        g.setObjet(new Cible(8, 5, g), 8, 5);
+        g.setObjet(new Boite(4, 1, g), 4, 1);
+        g.setObjet(new Boite(5, 3, g), 5, 3);
+        g.setObjet(new Boite(4, 5, g), 4, 5);
+        g.setObjet(new Joueur(1, 3, g), 1, 3);
 
+
+
+
+        BorderPane home = new BorderPane();
+        GridPane game = new GridPane();
+        Jeu jeu = new Jeu(g);
+        Level level = new Level(game, g, 5);
+
+        level.setBoard();
+        home.setCenter(game);
 
         Scene scene = new Scene(home, WIDTH, HEIGHT);
+
+        scene.setOnKeyPressed(e -> {
+
+            Direction direction = null;
+            boolean deplacementTente = false;
+
+            switch(e.getCode()){
+                case UP:
+                    direction = Direction.HAUT;
+                    deplacementTente = true;
+                    break;
+                case DOWN:
+                    direction = Direction.BAS;
+                    deplacementTente = true;
+                    break;
+                case LEFT:
+                    direction = Direction.GAUCHE;
+                    deplacementTente = true;
+                    break;
+                case RIGHT:
+                    direction = Direction.DROITE;
+                    deplacementTente = true;
+                    break;
+                case Z:
+                    if (jeu.annulerMouvement()) {
+                        level.updateBoardReverse();
+                    }
+                    break;
+            }
+
+            if (deplacementTente && direction != null) {
+                jeu.deplacerJoueur(direction);
+                level.updateBoard();
+
+                if (jeu.estNiveauTermine()) {
+                    System.out.println("vic");
+                }
+            }
+        });
+
+
         stage.setScene(scene);
         stage.show();
 
