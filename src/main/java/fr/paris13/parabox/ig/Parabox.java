99bbd161770cede635ce3fc9cea4c355ed66fc36
Léa Luu <lea.luu@edu.univ-paris13.fr>
2026-04-30 21:30:44 +0200

    ajout option auto et recommencer

--- src/main/java/fr/paris13/parabox/ig/Parabox.java
+++ src/main/java/fr/paris13/parabox/ig/Parabox.java
@@ -1,26 +1,39 @@
 package fr.paris13.parabox.ig;
 
+import fr.paris13.parabox.Modele.ChargeurNiveau;
 import fr.paris13.parabox.Modele.Direction;
 import fr.paris13.parabox.Modele.Grille;
 import fr.paris13.parabox.Modele.JeuRecursif;
+import fr.paris13.parabox.Modele.ParaboxLevel;
 import fr.paris13.parabox.Modele.Version;
+import fr.paris13.parabox.ResoAuto.PileDir;
+import fr.paris13.parabox.ResoAuto.ResoAutoRecursif;
 import fr.paris13.parabox.ig.menu.PauseMenu;
 import fr.paris13.parabox.ig.menu.VictoryMenu;
+import java.io.File;
+import javafx.animation.KeyFrame;
+import javafx.animation.PauseTransition;
+import javafx.animation.Timeline;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 
 public class Parabox extends StackPane {
     private final JeuRecursif jeu;
-    private Level level;
+    private final Level level;
     private final PauseMenu pause;
     private final StackPane root;
+    private final int lvl;
+    private final PileDir d;
     
-    public Parabox(Grille g, StackPane root){
+    public Parabox(Grille g, StackPane root, int lvl){
         this.root = root;
+        this.lvl = lvl;
+        d = ResoAutoRecursif.recursif(g, lvl);
         jeu = new JeuRecursif(g);
-        level = new Level(g);
+        level = new Level(g, lvl);
         level.setBoard();
         pause = new PauseMenu(root, Version.RECURSIVE);
         pause.setVisible(false);
@@ -34,7 +47,8 @@ public class Parabox extends StackPane {
     
     private void handleKeyInput(KeyCode code){
         Direction direction = null;
-
+        boolean auto = false;
+        
         switch(code){
             case UP:
                 direction = Direction.HAUT;
@@ -57,6 +71,44 @@ public class Parabox extends StackPane {
                     level.updateBoardRec(jeu.getGrilleActive());
                 }
                 break;
+            case R:
+                Grille ng = ParaboxLevel.getList()[lvl-1];
+                if(ng !=null) {
+                    jeu.reinitialiser(ng);
+                    level.reset(ng);
+                }
+                break;
+                
+            case A: // Auto
+                if (level.getLvl() >=2 && level.getLvl() <=3){
+                    Grille ng2 = ParaboxLevel.getList()[lvl-1];
+                    if(ng2 !=null) {
+                        jeu.reinitialiser(ng2);
+                        level.reset(ng2);
+                    }
+                    auto = true;
+                }
+                if(auto && !jeu.estNiveauTermine()) {
+                    int cmpt = d.size();
+                    Timeline t = new Timeline(new KeyFrame(Duration.seconds(0.5), 
+                            e -> {
+                                Direction dir2 = d.depiler2();
+                                jeu.deplacerJoueur(dir2);
+                                level.updateBoard(dir2);
+                                
+                                if (jeu.estNiveauTermine()) {
+                                    PauseTransition p = new PauseTransition(Duration.seconds(0.5));
+                                    p.setOnFinished(ev -> 
+                                        root.getChildren().setAll(new VictoryMenu(root, Version.RECURSIVE, level)));
+                                    p.play();
+                                }
+                            }
+                        ));
+                    t.setCycleCount(cmpt);
+                    t.play();
+                }
+                break;
+                
             case ESCAPE:
                 pause.setVisible(true);
         }
@@ -66,7 +118,7 @@ public class Parabox extends StackPane {
             if (ok){
                 level.updateBoardRec(jeu.getGrilleActive());
                 if (jeu.estNiveauTermine()) {
-                    root.getChildren().setAll(new VictoryMenu(root, Version.RECURSIVE));
+                    root.getChildren().setAll(new VictoryMenu(root, Version.RECURSIVE, level));
                 }
             }
         }    
